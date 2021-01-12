d3b1c2074bccc11c8b722cae8a1ece44d91d6f2d
TharraniJ <35766961+TharaniJ@users.noreply.github.com>
2021-01-12 19:36:35 +0530

    added all the newly created projected to the new repository

---
+++ Eclipse Projects/MvnFirstProject/mavenproject/src/test/java/com/tj/mavenproject/AppTest.java
@@ -0,0 +1,13 @@
+package com.tj.mavenproject;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+    
+    @Test
+    public void testHelloWorld()
+    {
+        System.out.println("Hello World");
+    }
+}
---
+++ Interllij projects/SeleniumFirstProject/src/test/java/TestJava.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.*;
+
+public class TestJava {
+
+    @Test
+    public void testHelloWorld()
+    {
+        System.out.println("Hello World");
+    }
+}
